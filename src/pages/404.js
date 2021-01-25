f888412263f0ef90a67796be3fc2e25cc1368ff9
Nick <nickeast1998@gmail.com>
2021-01-25 14:31:21 +0000

    triggering build

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,7 @@ import Message from '../components/Message';
 const ErrorPage = () => (
   <Layout>
     <Message
-      title="404"
+      title="404 "
       message="This page is no longer available or it has moved"
     />
   </Layout>
